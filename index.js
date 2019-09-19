a2500e0b83e5254018a451801bb15f6256bb789a
Nataliya R <n.rudenko@gmail.com>
2019-09-19 20:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(i) {
+function init() {
 console.log (this.firstChild.nodeValue.trim() + ' Awesome')
       for (let i = 0, l = code.length; i < l; i++) {
-        triggerKeyDown(code[i])}
+      }
  }
