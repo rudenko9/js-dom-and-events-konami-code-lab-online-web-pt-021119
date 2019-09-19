b4212bab7825ff1a2a8407390265cbbd173f7c41
Nataliya R <n.rudenko@gmail.com>
2019-09-19 20:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(i) {
-console.log (this)
+console.log ()
       for (let i = 0, l = code.length; i < l; i++) {
         triggerKeyDown(code[i])}
  }
