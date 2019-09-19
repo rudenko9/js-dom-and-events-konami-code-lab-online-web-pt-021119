d6418de07987c9012767d00163675436cef06a27
Nataliya R <n.rudenko@gmail.com>
2019-09-19 20:56:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
 console.log (this.firstChild.nodeValue.trim() + ' Awesome')
       for (let i = 0, l = code.length; i < l; i++) {
-      }
+       divs[i].addEventListener('keydown', 'Awesome'); }
  }
